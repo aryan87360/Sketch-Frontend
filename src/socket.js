8e3527d9dbcef5923a677639d81e5eda326aeb01
Aryan Sharma <62754988+aryan87360@users.noreply.github.com>
2023-12-24 01:06:47 +0530

    Add files via upload

---
+++ src/socket.js
@@ -0,0 +1,3 @@
+import { io } from "socket.io-client";
+const URL = process.env.NODE_ENV === 'production' ? 'https://sketch-backend-e8bx.onrender.com/' : 'http://localhost:5000'
+export const socket = io(URL);
